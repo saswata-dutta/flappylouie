ad37111b28f20ab563cd81e1f1360b033e4f68e8
Huiyeon Kim <gnldus59112@gmail.com>
2020-05-18 11:35:08 +0800

    try block mobile

--- libs/api.js
+++ libs/api.js
@@ -1,44 +1,70 @@
 let switchl = (function() {
-    if(sessionStorage.getItem('logged') === null) {
-        document.querySelector('.loggedout').style.display='flex'
-        document.querySelector('.loggedin').style.display='none'
-    } else {
-        document.querySelector('.loggedout').style.display='none'
-        document.querySelector('.loggedin').style.display='flex'
-        let name = sessionStorage.getItem('name')
-        let score = sessionStorage.getItem('score')
-        document.getElementById('w').innerHTML = `Welcome ${name}`
-        document.getElementById('s').innerHTML = `Your Max Score: ${score}`
-    }
+    if (navigator.userAgent.match(/Android/i) 
+        || navigator.userAgent.match(/webOS/i) 
+        || navigator.userAgent.match(/iPhone/i)  
+        || navigator.userAgent.match(/iPad/i)  
+        || navigator.userAgent.match(/iPod/i) 
+        || navigator.userAgent.match(/BlackBerry/i) 
+        || navigator.userAgent.match(/Windows Phone/i)) { 
+        a = true; 
+        document.querySelector('#no').style.display='flex'
+    } else { 
+        document.querySelector('#no').style.display='none'
+        a = false; 
+        if(sessionStorage.getItem('logged') === null) {
+            document.querySelector('.loggedout').style.display='flex'
+            document.querySelector('.loggedin').style.display='none'
+        } else {
+            document.querySelector('.loggedout').style.display='none'
+            document.querySelector('.loggedin').style.display='flex'
+            let name = sessionStorage.getItem('name')
+            let score = sessionStorage.getItem('score')
+            document.getElementById('w').innerHTML = `Welcome ${name}`
+            document.getElementById('s').innerHTML = `Your Max Score: ${score}`
+        }
+    } 
     });
 switchl();
 
 let populate = (async function() {
-    const response = await fetch(`https://www.flappybirdbackendapi.ml/scoreboard`, {
-    method: 'GET',
-    headers: {
-        'Content-Type': 'application/json'
-    }
-    });
-    const myJson = await response.json();
-    let tbody = document.getElementById('b')
-    let c = document.querySelectorAll('#b > tr');
-    for(let i of c) {
-        if(i.id === 'append') {
-            continue;
-        }
-        i.parentNode.removeChild(i);
-    }
-    for(let data of myJson){
-        let tr = document.createElement('tr');
-        let u = document.createElement('td');
-        let s = document.createElement('td');
+    if (navigator.userAgent.match(/Android/i) 
+        || navigator.userAgent.match(/webOS/i) 
+        || navigator.userAgent.match(/iPhone/i)  
+        || navigator.userAgent.match(/iPad/i)  
+        || navigator.userAgent.match(/iPod/i) 
+        || navigator.userAgent.match(/BlackBerry/i) 
+        || navigator.userAgent.match(/Windows Phone/i)) { 
+        a = true; 
+        document.querySelector('#no').style.display='flex'
+    } else {
+        document.querySelector('#no').style.display='none'
 
-        u.innerHTML = data[0];
-        s.innerHTML = data[1];
-        tr.appendChild(u)
-        tr.appendChild(s)
-        tbody.appendChild(tr)
+        const response = await fetch(`https://www.flappybirdbackendapi.ml/scoreboard`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        });
+        const myJson = await response.json();
+        let tbody = document.getElementById('b')
+        let c = document.querySelectorAll('#b > tr');
+        for(let i of c) {
+            if(i.id === 'append') {
+                continue;
+            }
+            i.parentNode.removeChild(i);
+        }
+        for(let data of myJson){
+            let tr = document.createElement('tr');
+            let u = document.createElement('td');
+            let s = document.createElement('td');
+    
+            u.innerHTML = data[0];
+            s.innerHTML = data[1];
+            tr.appendChild(u)
+            tr.appendChild(s)
+            tbody.appendChild(tr)
+        }
     }
 })
 
